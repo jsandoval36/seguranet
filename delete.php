2ac5e4fa9e233b447b1d5317cc8fa20335cc797e
Lorena Salazar <las346790@gmail.com>
2026-03-05 16:51:30 -0500

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["user_id"])) {
+  header("Location: index.html");
+  exit();
+}
+
+if (isset($_GET["file"])) {
+
+  $file = basename($_GET["file"]);
+  $path = __DIR__ . "/uploads/" . $file;
+
+  if (file_exists($path)) {
+    unlink($path);
+  }
+}
+
+header("Location: dashboard.php");
+exit();
+?>
+
