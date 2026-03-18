36432db61fa772013a0951b2a7053031323f481b
Tobi O <oodeyemi@ggc.edu>
2026-03-18 15:41:35 -0400

    Update database.php

--- database.php
+++ database.php
@@ -6,7 +6,7 @@ $db   = getenv("DB_NAME");
 
 try {
     // This is the specific connection string format for Azure SQL (Microsoft)
-    $conn = new PDO("sqlsrv:server = tcp:$host,1433; Database = $db", $user, $pass);
+    $conn = new PDO("odbc:Driver={ODBC Driver 17 for SQL Server};Server=tcp:$host,1433;Database=$db", $user, $pass);
     
     // Tell PDO to throw an exception if it encounters an error
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
