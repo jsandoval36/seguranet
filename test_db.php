c941ea445c32e770e11741de5705d3bf080afe95
jsandoval36 <138029555+jsandoval36@users.noreply.github.com>
2026-03-24 15:08:38 -0400

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -1,19 +1,11 @@
 <?php
 require_once "database.php";
 
-echo "<h3>Checking Environment Variables...</h3>";
-
 echo "DB_HOST: " . getenv("DB_HOST") . "<br>";
 echo "DB_USER: " . getenv("DB_USER") . "<br>";
 echo "DB_NAME: " . getenv("DB_NAME") . "<br><br>";
 
-echo "<h3>Connection Test:</h3>";
-
-try {
-    if ($conn) {
-        echo "✅ Database connected successfully!";
-    }
-} catch (Exception $e) {
-    echo "❌ Error: " . $e->getMessage();
+if ($conn) {
+    echo "✅ Database connected successfully!";
 }
 ?>
