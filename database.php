2c7dd17b4d3d2b488b09ed02d6c781529badd0fe
jsandoval36 <138029555+jsandoval36@users.noreply.github.com>
2026-03-24 15:04:59 -0400

    Update database.php

--- database.php
+++ database.php
@@ -5,7 +5,7 @@ $pass = getenv("DB_PASS");
 $db   = getenv("DB_NAME");
 
 try {
-    $conn = new PDO("sqlsrv:server = tcp:seguranet.database.windows.net,1433; Database = seguranetDB", "CloudSAf51fec62", "{Apple123!}");
+    $conn = new PDO("sqlsrv:server = tcp:seguranet.database.windows.net,1433; Database = seguranetDB", "ApplicationUser", "{Apple123!}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
