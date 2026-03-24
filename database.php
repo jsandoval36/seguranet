842c9f8cc5a8c93e5e8fc8c6a08a816b729b6bd8
jsandoval36 <138029555+jsandoval36@users.noreply.github.com>
2026-03-24 15:03:49 -0400

    Update database.php

--- database.php
+++ database.php
@@ -5,15 +5,13 @@ $pass = getenv("DB_PASS");
 $db   = getenv("DB_NAME");
 
 try {
-    $conn = new PDO(
-        "sqlsrv:Server=tcp:$host,1433;Database=$db",
-        $user,
-        $pass
-    );
-
+    $conn = new PDO("sqlsrv:server = tcp:seguranet.database.windows.net,1433; Database = seguranetDB", "CloudSAf51fec62", "{Apple123!}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
     die("Database connection failed: " . $e->getMessage());
 }
 ?>
